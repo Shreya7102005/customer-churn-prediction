4c530a22c2c986a5385685ab8dea1fb4dec60742
Shreya <shreyakumari.7102005@gmail.com>
2026-05-01 18:26:12 +0530

    append 2

--- End-to-End Customer Churn Prediction System/src/data_preprocessing.py
+++ End-to-End Customer Churn Prediction System/src/data_preprocessing.py
@@ -5,7 +5,17 @@ def load_data():
     df=pd.read_csv(data_path)
     return df
 
+def clean_data(df):
+    df['TotalCharges']=pd.to_numeric(df['TotalCharges'],errors='coerce')
+    df['TotalCharges'].fillna(df['TotalCharges'].median(),inplace=True)
+    df.drop('customerID',axis=1,inplace=True)
+    df['Churn']=df['Churn'].map({'Yes':1,'No':0})
+    return df 
+
 if __name__=="__main__":
     df=load_data()
+    df=clean_data(df)
     #print(df.head())
+
     
+
