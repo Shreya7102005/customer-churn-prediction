8360a4ccffff7dec3452c9843d9bbd2407b2ef94
Shreya <shreyakumari.7102005@gmail.com>
2026-05-03 23:43:39 +0530

    g

--- End-to-End Customer Churn Prediction System/src/data_preprocessing.py
+++ End-to-End Customer Churn Prediction System/src/data_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score
 
 data_path="data/Telco-Customer-Churn.csv"
 
@@ -58,4 +59,9 @@ if __name__=="__main__":
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
 
-    print("Sample Predictions:", y_pred[:10])
+    #print("Sample Predictions:", y_pred[:10])
+
+    print("\nAccuracy:", accuracy_score(y_test, y_pred))
+
+    print("\nClassification Report:\n")
+    print(classification_report(y_test, y_pred))
