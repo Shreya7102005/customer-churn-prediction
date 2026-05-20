7268112704df6ce11e885d423c9f3e65329b9d24
Shreya <shreyakumari.7102005@gmail.com>
2026-05-20 16:40:45 +0530

    project

---
+++ End-to-End Customer Churn Prediction System/src/predict.py
@@ -0,0 +1,56 @@
+import joblib
+import pandas as pd
+
+
+# Load saved model and column structure
+model = joblib.load("models/churn_model.pkl")
+model_columns = joblib.load("models/model_columns.pkl")
+
+
+def predict_churn(customer_data):
+    df = pd.DataFrame([customer_data])
+
+    # Binary encoding
+    binary_cols = ['Partner', 'Dependents', 'PhoneService', 'PaperlessBilling']
+
+    for col in binary_cols:
+        df[col] = df[col].map({'Yes': 1, 'No': 0})
+
+    # One-hot encoding
+    df = pd.get_dummies(df)
+
+    # Match training columns
+    df = df.reindex(columns=model_columns, fill_value=0)
+
+    prediction = model.predict(df)
+
+    return prediction[0]
+
+
+# Test prediction
+sample_customer = {
+    'gender': 'Female',
+    'SeniorCitizen': 0,
+    'Partner': 'Yes',
+    'Dependents': 'No',
+    'tenure': 5,
+    'PhoneService': 'Yes',
+    'MultipleLines': 'No',
+    'InternetService': 'Fiber optic',
+    'OnlineSecurity': 'No',
+    'OnlineBackup': 'Yes',
+    'DeviceProtection': 'No',
+    'TechSupport': 'No',
+    'StreamingTV': 'Yes',
+    'StreamingMovies': 'No',
+    'Contract': 'Month-to-month',
+    'PaperlessBilling': 'Yes',
+    'PaymentMethod': 'Electronic check',
+    'MonthlyCharges': 85.5,
+    'TotalCharges': 400.0
+}
+
+
+result = predict_churn(sample_customer)
+
+print("Prediction:", result)
